bd1b29898b32de13959950117121862b74d88099
cautiouspotato <afshaanlondhe4@gmail.com>
2018-03-28 13:57:55 +0530

    10 led's ha ha ha ha

--- ledwave/ledwave.ino
+++ ledwave/ledwave.ino
@@ -1,12 +1,16 @@
 // turns on and then off, a line of LED's one after the other, making a wave
 
 //set up the variables for the leds
-const int LED1 = 3;
-const int LED2 = 5;
-const int LED3 = 6;
-const int LED4 = 9;
-const int LED5 = 10;
-const int LED6 = 11;
+const int LED1 = 2;
+const int LED2 = 3;
+const int LED3 = 4;
+const int LED4 = 5;
+const int LED5 = 6;
+const int LED6 = 7;
+const int LED7 = 8;
+const int LED8 = 9;
+const int LED9 = 10;
+const int LED10 = 11;
 
 //set all the led pins as outputs
 void setup() {
@@ -16,6 +20,10 @@ void setup() {
   pinMode(LED4, OUTPUT);
   pinMode(LED5, OUTPUT);
   pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
+  pinMode(LED8, OUTPUT);
+  pinMode(LED9, OUTPUT);
+  pinMode(LED10, OUTPUT);
 }
 
 /*
@@ -36,6 +44,14 @@ void loop() {
   delay(500);
   digitalWrite(LED6, HIGH);
   delay(500);
+  digitalWrite(LED7, HIGH);
+  delay(500);
+  digitalWrite(LED8, HIGH);
+  delay(500);
+  digitalWrite(LED9, HIGH);
+  delay(500);
+  digitalWrite(LED10, HIGH);
+  delay(500);
   digitalWrite(LED1, LOW);
   delay(500);
   digitalWrite(LED2, LOW);
@@ -47,5 +63,13 @@ void loop() {
   digitalWrite(LED5, LOW);
   delay(500);
   digitalWrite(LED6, LOW);
+  delay(500);
+  digitalWrite(LED7, LOW);
+  delay(500);
+  digitalWrite(LED8, LOW);
+  delay(500);
+  digitalWrite(LED9, LOW);
+  delay(500);
+  digitalWrite(LED10, LOW);
   delay(500);
 }
