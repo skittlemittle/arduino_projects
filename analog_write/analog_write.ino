7bc6688944316d0db6aae34b956b4110875a7ebe
cautiouspotato <afshaanlondhe4@gmail.com>
2018-04-05 10:31:49 +0530

    add serial communicatiications

---
+++ analog_write/analog_write.ino
@@ -0,0 +1,24 @@
+// Fades an LED in and out, like a sleeping laptop
+
+const int LED = 9;
+int i = 0; //used to count up and down
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // loop from 1 to 255 and increase the 
+  // brightness of the LED each time we go through the loop
+  for (i = 0; i < 255; i++) { 
+    analogWrite(LED, i);
+    delay(10);
+  }
+
+  // loop from 1 to 255 and decrease the 
+  // brightness of the LED each time we go through the loop
+  for (i = 255; i > 0; i--) {
+    analogWrite(LED, i);
+    delay(10);
+  }
+}
