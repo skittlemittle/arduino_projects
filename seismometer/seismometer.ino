8e9e665b59a129967a06d009e5b11ce606a30269
cautiouspotato <afshaanlondhe4@gmail.com>
2018-04-15 11:06:43 +0530

    clean up seismogram code

--- seismometer/seismometer.ino
+++ seismometer/seismometer.ino
@@ -3,10 +3,10 @@
  This is not scientific or accurate, it just shows if there 
  is vibration and how much in raw analog signals.
 
+ the sensor is connected to analog in 0.
+
  Open the serial plotter in the Arduino IDE for nice graphs.
 */
-
-const int sensor = 0;
 
 int val = 0;
 int reading = 0; // used to map the readings from sensor to a range of 0 to 100.
@@ -18,7 +18,7 @@ void setup() {
 }
 
 void loop() {
-	val = analogRead(sensor); // read the sensor value.
+	val = analogRead(0); // read the sensor value.
 	/*
 	 val varies a lot, from 0 to 500, so to make the graphs a bit neater
 	 and so the vibrations are apparent, we map the readings from a range of 
